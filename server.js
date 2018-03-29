6fd72fb50baf72b8174ac2a018198f67a3ebf5aa
Alliance <574014131@qq.com>
2018-03-29 20:57:52 +0800

    --

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express=require('express')
 var fs=require('fs')
 var app=express()
-var userNames=[]
+var liveAddress=null
 app.get('/',function (req, res) {
     fs.readFile('./index.html',function (error,data) {
         res.writeHead(200,{'Content-Type':'text/html'})
@@ -10,4 +10,20 @@ app.get('/',function (req, res) {
 })
 var server=app.listen(3003,function () {
     console.log('Server is listening at http://localhost:3003')
+})
+var io=require('socket.io').listen(server)
+io.on('connection',function (socket) {
+    if(liveAddress !== null){
+        socket.emit('welcome',liveAddress)
+    }
+    socket.on('live',function (data) {
+        liveAddress=data
+    })
+    socket.on('watcher',function (data) {
+        // watchers.push(data)
+        io.emit('watcherAnswer',data)
+    })
+    socket.on('disconnect',function () {
+        console.log("user has left.")
+    })
 })
