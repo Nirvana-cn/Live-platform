6e7c5a622981487239c8714bb659f8d8a6232ff8
Alliance <574014131@qq.com>
2018-04-03 20:35:57 +0800

    --

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express=require('express')
 var fs=require('fs')
 var app=express()
+var liveSDP=null
 var liveAddress=null
+var userNames=[]
 app.get('/',function (req, res) {
     fs.readFile('./index.html',function (error,data) {
         res.writeHead(200,{'Content-Type':'text/html'})
@@ -13,23 +15,47 @@ var server=app.listen(3003,function () {
 })
 var io=require('socket.io').listen(server)
 io.on('connection',function (socket) {
-    if(liveAddress !== null){
-        socket.emit('welcome',liveAddress)
-    }
-    socket.on('live',function (data) {
-        liveAddress=data
+    socket.on('userName',function (data, callback) {
+        if(userNames.indexOf(data)!== -1){
+            callback(false)
+        }else{
+            callback(true)
+            if(data==='wuwei'){
+                liveAddress=socket.id
+                socket.emit('live')
+            }else{
+                userNames.push(data)
+                socket.userName=data
+                socket.emit('watch',liveSDP)
+                io.to(liveAddress).emit('addWatcher')
+            }
+        }
     })
+
+
+    socket.on('sendOffer',function (data) {
+        liveSDP=data
+    })
+
+    socket.on('sendAnswer',function (data) {
+        io.to(liveAddress).emit('watcherAnswer',data)
+    })
+
     socket.on('anchorIce',function (data) {
         socket.broadcast.emit('receiveAnchorIce',data)
     })
     socket.on('watcherIce',function (data) {
-        socket.broadcast.emit('receiveWatcherIce',data)
+        io.to(liveAddress).emit('receiveWatcherIce',data)
     })
-    socket.on('watcher',function (data) {
-        // watchers.push(data)
-        io.emit('watcherAnswer',data)
-    })
+
     socket.on('disconnect',function () {
-        console.log("user has left.")
+        console.log(socket.userName+" has left.")
+        if(!socket.userName) return;
+        if(userNames.indexOf(socket.userName)>-1){
+            userNames.splice(userNames.indexOf(socket.userName),1)
+            console.log(userNames)
+        }
+        socket.broadcast.emit('userNames',userNames)
+        io.to(liveAddress).emit('watcherLeave')
     })
 })
